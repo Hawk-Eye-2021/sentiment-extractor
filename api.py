b560326488e55a0f8e9c4e69e1b2b59e501a8103
mariano.longo <mariano.longo@ing.austral.edu.ar>
2022-01-17 17:20:20 -0300

    First Commit

---
+++ api.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_restful import Api
+from controllers import helloController
+
+
+app = Flask(__name__)
+
+api = Api(app)
+
+
+api.add_resource(helloController.HelloController, '/api/hello')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+yum -y update
+yum -y install git
+git clone https://github.com/Hawk-Eye-2021/sentiment-extractor.git
+cd content-processor
+
+# add swap
+dd if=/dev/zero of=/var/swap.1 bs=1M count=8192
+chmod 600 /var/swap.1
+mkswap /var/swap.1
+swapon /var/swap.1
+
+
+pip3 install -r requirements.txt --no-cache-dir
+
+python3 api.py
