09ae35ce3546e86a6abc32ea90e4350b41273f27
mariano.longo <mariano.longo@ing.austral.edu.ar>
2022-03-28 15:58:49 -0300

    changed build.sh to persist when rebooting

--- build.sh
+++ build.sh
@@ -1,5 +1,23 @@
-#!/bin/bash
+Content-Type: multipart/mixed; boundary="//"
+MIME-Version: 1.0
 
+--//
+Content-Type: text/cloud-config; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment; filename="cloud-config.txt"
+
+#cloud-config
+cloud_final_modules:
+- [scripts-user, always]
+
+--//
+Content-Type: text/x-shellscript; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment; filename="userdata.txt"
+
+#!/bin/bash
 yum -y update
 yum -y install git
 git clone https://github.com/Hawk-Eye-2021/sentiment-extractor.git
@@ -15,3 +33,4 @@ swapon /var/swap.1
 pip3 install -r requirements.txt --no-cache-dir
 
 python3 api.py
+--//--
